71c05a5020b2098c07ca73d0bb3ec4da03513732
sciomathman <sciomathman@gmail.com>
2018-03-22 16:23:44 +0530

    Added for site publishing

---
+++ data/twittertoken.js
@@ -0,0 +1,209 @@
+var tw2 = '<!DOCTYPE html>'+
+'<!-- saved from url=(0042)https://apps.twitter.com/xxx/keys -->'+
+'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr" xmlns:og="http://ogp.me/ns#" class="js"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'+
+'    '+
+'<meta name="twitter:title" content="Twitter Application Management">'+
+'<meta name="twitter:url" content="https://apps.twitter.com/xxx/keys">'+
+'<meta name="twitter:creator" content="@twitterapi">'+
+'<meta name="twitter:description" content="Application registration & configuration for developers using the Twitter REST & Streaming APIs.">'+
+'<meta name="twitter:creator:id" content="6253282">'+
+'<meta name="twitter:image" content="https://pbs.twimg.com/profile_images/2284174872/7df3h38zabcvjylnyfe3.png">'+
+'<script async="" src="data/twitterAppMngt_files/analytics.js"></script><script type="application/json" id="drupal-settings">{"basePath":"\/","pathPrefix":"","ajaxPageState":{"theme":"gazebo_apps","theme_token":"#"},"urlIsAjaxTrusted":{"\/app\/#\/keys":true},"analyticsSettings":{"google":{"accounts":["UA-30775-44","UA-30775-55"],"options":{"customVars":{"dimension2":"2"},"customUrl":"","trackerUrl":"https:\/\/g.twimg.com\/googleanalytics\/analytics.js","domains":["apps.twitter.com"],"cross_domains":[],"displayAdvertisingFeatures":true}},"scribe":{"client":"gazebo"},"trackingPixel":{"pixel_ids":[]},"bing":false}}</script><meta content="on" name="twitter:widgets:csp">'+
+'<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">'+
+'<link href="https://abs.twimg.com/favicons/favicon.ico" rel="shortcut icon" type="image/x-icon">'+
+'<meta name="twitter:site:id" content="6253282">'+
+'<meta name="twitter:card" content="summary">'+
+'<meta name="generator" content="Drupal 7 (http://drupal.org)">'+
+'<meta name="abstract" content="Application registration & configuration for developers using the Twitter REST & Streaming APIs.">'+
+'<meta name="description" content="Application registration & configuration for developers using the Twitter REST & Streaming APIs.">'+
+'<link href="https://g.twimg.com/ios_homescreen_icon.png" rel="apple-touch-icon">'+
+'<link rel="canonical" href="https://apps.twitter.com/xxx/keys">'+
+'<meta name="og:site_name" content="Twitter Application Management">'+
+'<meta name="og:url" content="https://apps.twitter.com/xxx/keys">'+
+'<meta name="og:image" content="https://pbs.twimg.com/profile_images/2284174872/7df3h38zabcvjylnyfe3.png">'+
+'<meta name="og:type" content="article">'+
+'<meta name="og:title" content="Access denied">'+
+'<meta name="twitter:site" content="@twitterapi">'+
+'    <title>Access denied | Twitter Application Management</title>'+
+'    <link type="text/css" rel="stylesheet" href="data/twitterAppMngt_files/css_QYpWDH1MckmIgh6wFtawzCJx0P4GpxG7vFDvXajoV7Q.css" media="all">'+
+'<link type="text/css" rel="stylesheet" href="data/twitterAppMngt_files/css_sIhfd1AIaScyZ0Ro3yeD8e_lv9OHNmlgCWidBHRlOEc.css" media="all">'+
+'<link type="text/css" rel="stylesheet" href="data/twitterAppMngt_files/css_kvMYhpy86KXrTv6PTJP566bT2Oa7pUIoSGivlHEMLE0.css" media="all">'+
+'<link type="text/css" rel="stylesheet" href="data/twitterAppMngt_files/css_99MuNsPgao6QXNEC_pC8QhxKxjZtCyS6Iz0anAvBwFQ.css" media="all">'+
+'    <script type="text/javascript" src="data/twitterAppMngt_files/js_kYMI6stYSsIRxnH5jlyWN3v76pk66IDAcsvBOk1W_qc.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_khfOAWT2z7QE7s9bNkw5afNKRe9w8PMP6gthYSvApG0.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_UTyslL_tPc0ipuhvkBtUjjFxNdFCZ3z_qn7A_R1RZ-Q.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_DvHwU1y7cKtYNAgFPchBYFCDPsQWlMU16N-1nX_0IEk.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_gWzLjJQwd6btPYevFzS6tn23govMOUXPbvEpq2Lq7no.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_BsD1J0VYO_7sNrnLQ25AQi_96pz4TWChe8TDkeZN9vs.js"></script>'+
+'  </head>'+
+''+
+'  <body class="html not-front logged-in no-sidebars page-app page-app- page-app-14948250 page-app-keys i18n-en">'+
+'    <div id="skip-link">'+
+'      <a href="https://apps.twitter.com/xxx/keys#main-content" class="element-invisible element-focusable">Skip to main content</a>'+
+'    </div>'+
+'        <div id="page-wrapper">'+
+'  <!-- Code for the global nav -->'+
+'      '+
+'    <nav id="globalnav" class="without-subnav">'+
+'      <div class="container">'+
+'        <div id="site-name">'+
+'          <a href="https://apps.twitter.com/" title="Home" class="pull-left">'+
+'            <span class="twitter-bird"></span>'+
+'            Application Management          </a>'+
+'                  </div>'+
+'        <div class="d-block d-block-menu signed-in dropdown profile-menu">'+
+'  <a class="profile-menu-handle dropdown-toggle" data-toggle="dropdown" href="http://twitter.com/twitterid">'+
+'    <div class="picture"></div>'+
+'  </a>'+
+'<ul class="menu dropdown-menu pull-right" role="menu"><li class="first leaf" role="menuitem"><a href="https://dev.twitter.com/">Back to dev.twitter.com</a></li>'+
+'<li class="leaf" role="menuitem"><a href="https://twitter.com/">Back to Twitter</a></li>'+
+'<li class="last leaf" role="menuitem"><a href="https://twitter.com/logout">Sign out</a></li>'+
+'</ul></div>      </div>'+
+'    </nav>'+
+''+
+'    <nav id="subnav">'+
+'        </nav>'+
+'  '+
+'  '+
+'  '+
+'  <section id="hero" class="hero-short">'+
+'    <div class="hero-wrap-outer">'+
+'          </div>'+
+'  </section>'+
+''+
+'  <section id="gaz-content">'+
+'    <div class="container">'+
+'      '+
+'            <div id="messages">'+
+'        <div class="alert alert-error"><h4>Status</h4>Your application access token has been successfully generated. It may take a moment for changes you\'ve made to reflect.<br><a href="https://apps.twitter.com/xxx/keys" class="active">Refresh</a> if your changes are not yet indicated.</div>      </div>'+
+'      '+
+'      <div id="gaz-content-wrap-outer" class="row">'+
+''+
+'        '+
+'        <div id="gaz-content-wrap-inner" class="span12">'+
+''+
+'          '+
+'          <div class="row">'+
+''+
+'            <div class="article-wrap span12">'+
+''+
+'              '+
+'              <article id="gaz-content-body" class="content">'+
+'                              <header>'+
+'                  <h1 id="page-title">App Name</h1>'+
+'                </header>'+
+'              '+
+'                            <div class="header-action">'+
+'                <a href="https://dev.twitter.com/apps/14948250/oauth" class="btn">Test OAuth</a>              </div>'+
+'              '+
+'                            <div class="tabs">'+
+'                <h2 class="element-invisible">Primary tabs</h2><ul class="nav nav-tabs"><li><a href="https://apps.twitter.com/xxx">Details</a></li>'+
+'<li><a href="https://apps.twitter.com/xxx/settings">Settings</a></li>'+
+'<li class="active"><a href="https://apps.twitter.com/xxx/keys" class="active">Keys and Access Tokens<span class="element-invisible">(active tab)</span></a></li>'+
+'<li><a href="https://apps.twitter.com/xxx/permissions">Permissions</a></li>'+
+'</ul>              </div>'+
+'              '+
+'              <div class="d-block d-block-system g-main">'+
+'        '+
+'<div class="app-details">'+
+'  <h2>Application Settings</h2>'+
+'  <div class="description">Keep the "Consumer Secret" a secret. This key should never be human-readable in your application.</div>'+
+'  <div class="app-settings">'+
+'    <div class="row">'+
+'      <span class="heading">Consumer Key (API Key)</span>'+
+'      <span>#########################</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Consumer Secret (API Secret)</span>'+
+'      <span>**************************************************</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Access Level</span>'+
+'      <span>'+
+'        Read and write        <span>(<a href="https://apps.twitter.com/xxx/permissions">modify app permissions</a>)</span>'+
+'              </span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Owner</span>'+
+'      <span>Owner ID</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Owner ID</span>'+
+'      <span>ID################</span>'+
+'    </div>'+
+'    <form action="https://apps.twitter.com/xxx/keys" method="post" id="twitter-apps-keys-form" accept-charset="UTF-8"><div><fieldset class="form-wrapper" id="edit-app-actions"><legend><span class="fieldset-legend">Application Actions</span></legend><div class="fieldset-wrapper"><a href="https://apps.twitter.com/xxx/recreate_keys" class="btn">Regenerate Consumer Key and Secret</a><a href="https://apps.twitter.com/xxx/permissions" class="btn">Change App Permissions</a></div></fieldset>'+
+'<input name="form_build_id" value="form-JnvSXWnyWfzV4b8M_urs3Xp_5wVDDNldhAI5m9G6NI0" type="hidden">'+
+'<input name="form_token" value="callback" type="hidden">'+
+'<input name="form_id" value="twitter_apps_keys_form" type="hidden">'+
+'</div></form>  </div>'+
+''+
+'  <h2>Your Access Token</h2>'+
+'    <div class="description">'+
+'    This access token can be used to make API requests on your own '+
+'account\'s behalf. Do not share your access token secret with anyone.  </div>'+
+'  <div class="access">'+
+'    <div class="row">'+
+'      <span class="heading">Access Token</span>'+
+'      <span>##################################################</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Access Token Secret</span>'+
+'      <span>##################################################</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Access Level</span>'+
+'      <span>Read and write</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Owner</span>'+
+'      <span>IDxxxxxxxxxxx</span>'+
+'    </div>'+
+'    <div class="row">'+
+'      <span class="heading">Owner ID</span>'+
+'      <span>*********************</span>'+
+'    </div>'+
+'  </div>'+
+'    <form action="https://apps.twitter.com/xxx/keys" method="post" id="twitter-apps-form-owner-token" accept-charset="UTF-8"><div><fieldset class="form-wrapper" id="edit-token-actions"><legend><span class="fieldset-legend">Token Actions</span></legend><div class="fieldset-wrapper"><a href="#" class="btn">Regenerate My Access Token and Token Secret</a><a href="#" class="btn">Revoke Token Access</a></div></fieldset>'+
+'<input name="form_build_id" value="form-HBjrCKbUU0NrE-sLRgBpuJeSVT7e5GpuPTqoQJfFe0U" type="hidden">'+
+'<input name="form_token" value="callback" type="hidden">'+
+'<input name="form_id" value="twitter_apps_form_owner_token" type="hidden">'+
+'</div></form></div>'+
+'</div>              </article>'+
+''+
+'              '+
+'            </div>'+
+'            '+
+'          </div>'+
+'        </div>'+
+'      </div>'+
+'      '+
+'    </div>  </section>'+
+'  <footer id="gaz-sub-footer" class="row-fluid">'+
+'    <div class="container">'+
+'          </div>'+
+'  </footer>'+
+''+
+'  <!-- Code for the global footer -->'+
+'      <footer id="globalfooter" class="row-fluid">'+
+'      <div class="container">'+
+'        <div class="d-block d-block-menu g-menu-global-footer">'+
+'  <ul class="menu nav clearfix" role="menu"><li class="first leaf" role="menuitem"><a href="https://about.twitter.com/">About</a></li>'+
+'<li class="leaf" role="menuitem"><a href="https://twitter.com/tos">Terms</a></li>'+
+'<li class="leaf" role="menuitem"><a href="https://twitter.com/privacy">Privacy</a></li>'+
+'<li class="last leaf" role="menuitem"><a href="https://support.twitter.com/articles/20170514">Cookies</a></li>'+
+'</ul></div>        <span class="copyright">© 2018 Twitter, Inc.</span>'+
+'      </div>'+
+'    </footer>'+
+'  '+
+'</div>'+
+'    <script type="text/javascript" src="data/twitterAppMngt_files/js_Rk5YIkK6m2gKTGH-GrMu0pM_PlMKXnI0ktQUgXw1XgA.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/widgets.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_WRH7WYq-FQCJT26vr-LZAlWu_t37jPSQ1Sws4gdMfko.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/oct.js"></script>'+
+'<script type="text/javascript" src="data/twitterAppMngt_files/js_6yE-_uKeO7bidfCOj5hFbBaaInw0QU2U0ekyhBlHU9k.js"></script>'+
+'  '+
+''+
+'<iframe scrolling="no" allowtransparency="true" src="data/twitterAppMngt_files/widget_iframe.html" style="display: none;" frameborder="0"></iframe><iframe id="rufous-sandbox" scrolling="no" allowtransparency="true" allowfullscreen="true" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: none;" title="Twitter analytics iframe" src="data/twitterAppMngt_files/saved_resource.html" frameborder="0"></iframe>'+
+'<iframe id="rufous-sandbox" scrolling="no" allowtransparency="true" allowfullscreen="true" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: medium none;" title="Twitter analytics iframe" frameborder="0"></iframe></body></html>';
+	
+
