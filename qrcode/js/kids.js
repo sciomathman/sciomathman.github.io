954047e9516c35d5ae318f8ef7e58400664e176a
Sciomathman <sciomathman@gmail.com>
2023-12-22 19:47:00 +0530

    Add files via upload

---
+++ qrcode/js/kids.js
@@ -0,0 +1,54 @@
+var svg_front = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlin' xml:space='preserve'  width='200' height='140'  viewBox='0 0 200 140' zoomAndPan='enable' preserveAspectRatio='xMidYMin meet' ><image x='0' y='0' width='200px' height='140px' xlink:href='images/";
+var svg_rear = "'/></svg>";
+var questions = {
+	"1":{
+ 			"title":"Question 1 of 10:",
+ 			"question": svg_front + 'elephant.png' + svg_rear,
+ 			"options" : ["Elephant", "Dinosaur", "Mouse"]
+ 		},
+	"2":{
+ 			"title":"Question 2 of 10:",
+ 			"question": svg_front + 'wolf.png' + svg_rear,
+ 			"options" : ["Dog", "Wolf", "Fox"]
+ 		},
+	"3":{
+ 			"title":"Question 3 of 10:",
+ 			"question": svg_front + 'camel.png' + svg_rear,
+ 			"options" : ["horse", "kangaroo", "Camel"]
+ 		},
+	"4":{
+ 			"title":"Question 4 of 10:",
+ 			"question": svg_front + 'tiger.png' + svg_rear,
+ 			"options" : ["Tiger", "Zebra", "leopard"]
+ 		},	
+	"5":{
+ 			"title":"Question 5 of 10:",
+ 			"question": svg_front + 'lion.png' + svg_rear,
+ 			"options" : ["Wofl", "Lion", "Monkey"]
+ 		},	
+	"6":{
+ 			"title":"Question 6 of 10:",
+ 			"question": svg_front + 'fox.png' + svg_rear,
+ 			"options" : ["Bear", "Wolf", "Fox"]
+ 		},	
+	"7":{
+ 			"title":"Question 7 of 10:",
+ 			"question": svg_front + 'zebra.png' + svg_rear,
+ 			"options" : ["Horse", "Zebra", "Donkey"]
+ 		},	
+	"8":{
+ 			"title":"Question 8 of 10:",
+ 			"question": svg_front + 'cow.png' + svg_rear,
+ 			"options" : ["Cow", "Cat", "Camel"]
+ 		},
+	"9":{
+ 			"title":"Question 9 of 10:",
+ 			"question": svg_front + 'horse.png' + svg_rear,
+ 			"options" : ["Zebra", "Deer", "Horse"]
+ 		},
+	"10":{
+ 			"title":"Question 10 of 10:",
+ 			"question": svg_front + 'rabbit.png' + svg_rear,
+ 			"options" : ["Mouse", "Rabbit", "Cat"]
+ 		}	 			 			 		 		 		 			
+};
